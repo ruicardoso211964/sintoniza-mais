e180688bd079028a5998e67fc57a0cbf0146fa3a
Rui Cardoso - Locução <rclocucao@gmail.com>
2025-04-10 14:24:32 +0000

    feat: implemented fetch news from multiple feeds and display radio names and number of news

--- src/components/news-aggregator.tsx
+++ src/components/news-aggregator.tsx
@@ -1,4 +1,5 @@
-import React from 'react';import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import React, { useEffect, useState } from 'react';import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import Parser from 'rss-parser';
 
 interface NewsItem {
   title: string;
@@ -24,15 +25,61 @@ const mockNewsData: NewsItem[] = [
   },
 ];
 
+const rssFeeds = [
+  'https://radioamparo.pt/rss',
+  'https://mundialfm.sapo.pt/feed/',
+  'https://radiocaria.pt/feed/',
+  'https://www.radiomertola.pt/site/feed/',
+  'https://www.radioourique.com/site/feed/',
+  'http://radioideias.com.pt/sintralife/feed/',
+];
+
+async function fetchNewsFromFeeds(feeds: string[]): Promise<NewsItem[]> {
+  const parser = new Parser();
+  const newsFromAllFeeds: NewsItem[] = [];
+  const feedNames = ['Rádio Amparo', 'Mundial FM', 'Rádio Caria', 'Rádio Mértola', 'Rádio Ourique', 'Rádio Ideias'];
+
+  for (let i = 0; i < feeds.length; i++) {
+    const feedUrl = feeds[i];
+    const feedName = feedNames[i];
+    try {
+        const feed = await parser.parseURL(feedUrl);
+        console.log(`Feed ${feedName} tem ${feed.items.length} noticias`);
+        newsFromAllFeeds.push({
+            title: `Feed ${feedName}`,
+            description: `Foram encontradas ${feed.items.length} noticias`,
+            source: feedName
+          });
+      } catch (error) {
+        console.error(`Error fetching or parsing feed ${feedUrl}:`, error);
+      }
+
+  }
+
+
+  return newsFromAllFeeds;
+}
+
+
 const NewsAggregator: React.FC = () => {
+
+  const [newsData, setNewsData] = useState<NewsItem[]>(mockNewsData);
+
+
+  useEffect(() => {
+    fetchNewsFromFeeds(rssFeeds).then(news => {
+      setNewsData(news)
+    });
+  }, []);
+
   return (
     <section className="container mx-auto mt-8">
-      <div className="mb-6 px-4">
+      <div className="mb-6 px-4"> 
         <h2 className="text-3xl font-bold text-gray-800">Notícias Locais</h2>
       </div>
       <div className="grid grid-cols-1 gap-6 px-4 md:grid-cols-2 lg:grid-cols-3">
         {mockNewsData.map((news, index) => (
-          <Card key={index} className="shadow-md">
+          <Card key={index} className="shadow-md ">
             <CardHeader>
               <CardTitle className="text-lg font-semibold">{news.title}</CardTitle>
             </CardHeader>
